22cec67752e327700200037c19a2b5c31a333580
John Marsh <john.marsh@digitalturbine.com>
2019-03-11 19:15:38 -0400

    Expended the types of Lists, Maps, and Sets generated

--- src/main/java/com/threeleaf/test/random/util/TestListUtil.java
+++ src/main/java/com/threeleaf/test/random/util/TestListUtil.java
@@ -4,17 +4,20 @@ import io.github.benas.randombeans.api.EnhancedRandom;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 
-import java.util.List;
+import java.util.*;
 
+import static com.threeleaf.test.random.TestNumber.INT_01;
+import static com.threeleaf.test.random.TestNumber.INT_03;
 import static com.threeleaf.test.random.util.TestNumberUtil.random1to10;
+import static com.threeleaf.test.random.util.TestNumberUtil.randomBetween;
 
 @UtilityClass
-@SuppressWarnings({"WeakerAccess"})
+@SuppressWarnings({"WeakerAccess", "unused"})
 public class TestListUtil
 {
 
     /**
-     * Return a list of randomized objects.
+     * Return a {@link ArrayList} of randomized objects.
      *
      * @param size the number of objects in the list
      * @param type type of Class
@@ -22,9 +25,49 @@ public class TestListUtil
      *
      * @return the list of randomized objects
      */
-    public static <T> List<T> randomListOf(final int size, @NonNull Class<T> type)
+    private static <T> ArrayList<T> randomArrayListOf(final int size, final @NonNull Class<T> type)
     {
-        return EnhancedRandom.randomListOf(size, type);
+        return (ArrayList<T>) EnhancedRandom.randomListOf(size, type);
+    }
+
+    /**
+     * Return an {@link ArrayList} of 1 to 10 randomized objects.
+     *
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the list of randomized objects
+     */
+    public static <T> ArrayList<T> randomArrayListOf(@NonNull Class<T> type)
+    {
+        return randomArrayListOf(random1to10(), type);
+    }
+
+    /**
+     * Return a {@link LinkedList} of randomized objects.
+     *
+     * @param size the number of objects in the linked list
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the list of randomized objects
+     */
+    private static <T> LinkedList<T> randomLinkedListOf(final int size, final @NonNull Class<T> type)
+    {
+        return new LinkedList<>(EnhancedRandom.randomListOf(size, type));
+    }
+
+    /**
+     * Return an {@link LinkedList} of 1 to 10 randomized objects.
+     *
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the list of randomized objects
+     */
+    public static <T> LinkedList<T> randomLinkedListOf(@NonNull Class<T> type)
+    {
+        return randomLinkedListOf(random1to10(), type);
     }
 
     /**
@@ -38,5 +81,60 @@ public class TestListUtil
     public static <T> List<T> randomListOf(@NonNull Class<T> type)
     {
         return randomListOf(random1to10(), type);
+    }
+
+    /**
+     * Return a {@link List} of randomized objects.
+     *
+     * @param size the number of objects in the list
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the list of randomized objects
+     */
+    public static <T> List<T> randomListOf(final int size, @NonNull Class<T> type)
+    {
+        List<T> list;
+        switch (randomBetween(INT_01, INT_03))
+        {
+            case 1:
+                list = randomArrayListOf(size, type);
+                break;
+            case 2:
+                list = randomLinkedListOf(size, type);
+                break;
+            default:
+                list = randomVectorOf(size, type);
+                break;
+        }
+
+        return list;
+    }
+
+    /**
+     * Return an {@link Vector} of 1 to 10 randomized objects.
+     *
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the list of randomized objects
+     */
+    public static <T> Vector<T> randomVectorOf(@NonNull Class<T> type)
+    {
+        return randomVectorOf(random1to10(), type);
+    }
+
+    /**
+     * Return a {@link Vector} of randomized objects.
+     *
+     * @param size the number of objects in the list
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the list of randomized objects
+     */
+    private static <T> Vector<T> randomVectorOf(final int size, final @NonNull Class<T> type)
+    {
+        return new Vector<>(randomArrayListOf(size, type));
     }
 }
--- src/main/java/com/threeleaf/test/random/util/TestSetUtil.java
+++ src/main/java/com/threeleaf/test/random/util/TestSetUtil.java
@@ -4,17 +4,21 @@ import io.github.benas.randombeans.api.EnhancedRandom;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 
-import java.util.Set;
+import java.util.*;
 
+import static com.threeleaf.test.random.TestNumber.INT_01;
+import static com.threeleaf.test.random.TestNumber.INT_03;
 import static com.threeleaf.test.random.util.TestNumberUtil.random1to10;
+import static com.threeleaf.test.random.util.TestNumberUtil.randomBetween;
 
+/** Test Set Utilities. */
 @UtilityClass
-@SuppressWarnings({"WeakerAccess"})
+@SuppressWarnings({"WeakerAccess", "unused"})
 public class TestSetUtil
 {
 
     /**
-     * Return a set of  randomized objects.
+     * Return a {@link HashSet} of randomized objects.
      *
      * @param size size of set
      * @param type type of Class
@@ -22,13 +26,53 @@ public class TestSetUtil
      *
      * @return the set of randomized objects
      */
-    public static <T> Set<T> randomSetOf(final int size, @NonNull Class<T> type)
+    private static <T> HashSet<T> randomHashSetOf(final int size, final @NonNull Class<T> type)
     {
-        return EnhancedRandom.randomSetOf(size, type);
+        return (HashSet<T>) EnhancedRandom.randomSetOf(size, type);
     }
 
     /**
-     * Return a set of 1 to 10 randomized objects.
+     * Return a {@link HashSet} of 1 to 10 randomized objects.
+     *
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the set of randomized objects
+     */
+    private static <T> HashSet<T> randomHashSetOf(final @NonNull Class<T> type)
+    {
+        return (HashSet<T>) randomSetOf(random1to10(), type);
+    }
+
+    /**
+     * Return a {@link LinkedHashSet} of 1 to 10 randomized objects.
+     *
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the set of randomized objects
+     */
+    private static <T> HashSet<T> randomLinkedHashSetOf(final @NonNull Class<T> type)
+    {
+        return randomLinkedHashSetOf(random1to10(), type);
+    }
+
+    /**
+     * Return a {@link HashSet} of randomized objects.
+     *
+     * @param size size of set
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the set of randomized objects
+     */
+    private static <T> HashSet<T> randomLinkedHashSetOf(final int size, final @NonNull Class<T> type)
+    {
+        return new LinkedHashSet<>(randomHashSetOf(size, type));
+    }
+
+    /**
+     * Return a {@link Set} of 1 to 10 randomized objects.
      *
      * @param type type of Class
      * @param <T>  the Class
@@ -38,5 +82,87 @@ public class TestSetUtil
     public static <T> Set<T> randomSetOf(@NonNull Class<T> type)
     {
         return randomSetOf(random1to10(), type);
+    }
+
+    /**
+     * Return a {@link Set} of randomized objects.
+     *
+     * @param size size of set
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the set of randomized objects
+     */
+    public static <T> Set<T> randomSetOf(final int size, @NonNull Class<T> type)
+    {
+        Set<T> set;
+        switch (randomBetween(INT_01, INT_03))
+        {
+            case 1:
+                set = randomHashSetOf(size, type);
+                break;
+            case 2:
+                set = randomTreeSetOf(size, type);
+                break;
+            default:
+                set = randomLinkedHashSetOf(size, type);
+                break;
+        }
+
+        return set;
+    }
+
+    /**
+     * Return a {@link SortedSet} of randomized objects.
+     *
+     * @param size size of set
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the set of randomized objects
+     */
+    private static <T> SortedSet<T> randomSortedSetOf(final int size, final @NonNull Class<T> type)
+    {
+        return randomTreeSetOf(size, type);
+    }
+
+    /**
+     * Return a {@link SortedSet} of 1 to 10 randomized objects.
+     *
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the set of randomized objects
+     */
+    private static <T> SortedSet<T> randomSortedSetOf(final @NonNull Class<T> type)
+    {
+        return randomSortedSetOf(random1to10(), type);
+    }
+
+    /**
+     * Return a {@link TreeSet} of randomized objects.
+     *
+     * @param size size of set
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the set of randomized objects
+     */
+    private static <T> TreeSet<T> randomTreeSetOf(final int size, final @NonNull Class<T> type)
+    {
+        return new TreeSet<>(randomHashSetOf(size, type));
+    }
+
+    /**
+     * Return a {@link TreeSet} of 1 to 10 randomized objects.
+     *
+     * @param type type of Class
+     * @param <T>  the Class
+     *
+     * @return the set of randomized objects
+     */
+    private static <T> TreeSet<T> randomTreeSetOf(final @NonNull Class<T> type)
+    {
+        return randomTreeSetOf(random1to10(), type);
     }
 }
