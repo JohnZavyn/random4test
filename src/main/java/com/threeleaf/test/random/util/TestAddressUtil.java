4c524bd9463abe5468fbf9849a4fc0aba52c598f
John Marsh <john.marsh@digitalturbine.com>
2019-04-06 13:03:51 -0400

    Added state capital and state capital zip code information

--- src/main/java/com/threeleaf/test/random/util/TestAddressUtil.java
+++ src/main/java/com/threeleaf/test/random/util/TestAddressUtil.java
@@ -15,59 +15,99 @@ import static com.threeleaf.test.random.util.TestNumberUtil.randomBetween;
 public class TestAddressUtil
 {
 
-    /** A Map of state information including full name, minimum zip code, and maximum zip code (excludes some exceptions). */
+    /**
+     * A Map of state information including:
+     * <ul>
+     * <li>State abbreviation (key)</li>
+     * <li>State name</li>
+     * <li>Minimum zip code</li>
+     * <li>Maximum zip code</li>
+     * <li>State capital</li>
+     * <li>State capital primary zip code</li>
+     * </ul>
+     * Note that the min and max zip codes have been simplified to have a single range for each
+     * state. Some states have two or three ranges.
+     * <p>
+     * See http://www.aip2.com/zip2.htm
+     * https://www.irs.gov/pub/irs-utl/zip%20code%20and%20state%20abbreviations.pdf
+     */
     public static final Map<String, Object[]> STATE_INFORMATION = ImmutableMap.<String, Object[]>builder()  // @formatter:off //
-          .put("AL", new Object[]{"Alabama", 35004, 36925})
-          .put("AK", new Object[]{"Alaska", 99501, 99950})
-          .put("AZ", new Object[]{"Arizona", 85001, 86556})
-          .put("AR", new Object[]{"Arkansas", 71601, 72959})
-          .put("CA", new Object[]{"California", 90001, 96162})
-          .put("CO", new Object[]{"Colorado", 80001, 81658})
-          .put("CT", new Object[]{"Connecticut", 6001, 6928})
-          .put("DE", new Object[]{"Delaware", 19701, 19980})
-          .put("DC", new Object[]{"Dist of Columbia", 20001, 20799})
-          .put("FL", new Object[]{"Florida", 32004, 34997})
-          .put("GA", new Object[]{"Georgia", 30001, 31999})
-          .put("HI", new Object[]{"Hawaii", 96701, 96898})
-          .put("ID", new Object[]{"Idaho", 83201, 83876})
-          .put("IL", new Object[]{"Illinois", 60001, 62999})
-          .put("IN", new Object[]{"Indiana", 46001, 47997})
-          .put("IA", new Object[]{"Iowa", 50001, 52809})
-          .put("KS", new Object[]{"Kansas", 66002, 67954})
-          .put("KY", new Object[]{"Kentucky", 40003, 42788})
-          .put("LA", new Object[]{"Louisiana", 70001, 71497})
-          .put("ME", new Object[]{"Maine", 3901, 4992})
-          .put("MD", new Object[]{"Maryland", 20331, 21930})
-          .put("MA", new Object[]{"Massachusetts", 1001, 2791})
-          .put("MI", new Object[]{"Michigan", 48001, 49971})
-          .put("MN", new Object[]{"Minnesota", 55001, 56763})
-          .put("MS", new Object[]{"Mississippi", 38601, 39776})
-          .put("MT", new Object[]{"Montana", 59001, 59937})
-          .put("NE", new Object[]{"Nebraska", 68001, 69367})
-          .put("NV", new Object[]{"Nevada", 88901, 89883})
-          .put("NH", new Object[]{"New Hampshire", 3031, 3897})
-          .put("NJ", new Object[]{"New Jersey", 7001, 8989})
-          .put("NM", new Object[]{"New Mexico", 87001, 88441})
-          .put("NY", new Object[]{"New York", 10001, 14975})
-          .put("NC", new Object[]{"North Carolina", 27006, 28909})
-          .put("ND", new Object[]{"North Dakota", 58001, 58856})
-          .put("OH", new Object[]{"Ohio", 43001, 45999})
-          .put("OK", new Object[]{"Oklahoma", 73001, 74966})
-          .put("OR", new Object[]{"Oregon", 97001, 97920})
-          .put("PA", new Object[]{"Pennsylvania", 15001, 19640})
-          .put("RI", new Object[]{"Rhode Island", 2801, 2940})
-          .put("SC", new Object[]{"South Carolina", 29001, 29948})
-          .put("SD", new Object[]{"South Dakota", 57001, 57799})
-          .put("TN", new Object[]{"Tennessee", 37010, 38589})
-          .put("TX", new Object[]{"Texas", 75001, 79999})
-          .put("UT", new Object[]{"Utah", 84001, 84784})
-          .put("VT", new Object[]{"Vermont", 5001, 5907})
-          .put("VA", new Object[]{"Virginia", 20040, 24658})
-          .put("WA", new Object[]{"Washington", 98001, 99403})
-          .put("WV", new Object[]{"West Virginia", 24701, 26886})
-          .put("WI", new Object[]{"Wisconsin", 53001, 54990})
-          .put("WY", new Object[]{"Wyoming", 82001, 83128})
+          .put("AL", new Object[]{"Alabama",          35004, 36925, "Montgomery",     "36101" })
+          .put("AK", new Object[]{"Alaska",           99501, 99950, "Juneau",         "99801" })
+          .put("AZ", new Object[]{"Arizona",          85001, 86556, "Phoenix",        "85001" })
+          .put("AR", new Object[]{"Arkansas",         71601, 72959, "Little Rock",    "72201" })
+          .put("CA", new Object[]{"California",       90001, 96162, "Sacramento",     "94203" })
+          .put("CO", new Object[]{"Colorado",         80001, 81658, "Denver",         "80201" })
+          .put("CT", new Object[]{"Connecticut",       6001,  6928, "Hartford",       "06101" })
+          .put("DE", new Object[]{"Delaware",         19701, 19980, "Dover",          "19901" })
+          .put("DC", new Object[]{"Dist of Columbia", 20001, 20799, "Washington",     "20001" })
+          .put("FL", new Object[]{"Florida",          32004, 34997, "Tallahassee",    "32301" })
+          .put("GA", new Object[]{"Georgia",          30001, 31999, "Atlanta",        "30301" })
+          .put("HI", new Object[]{"Hawaii",           96701, 96898, "Honolulu",       "96801" })
+          .put("ID", new Object[]{"Idaho",            83201, 83876, "Boise",          "83701" })
+          .put("IL", new Object[]{"Illinois",         60001, 62999, "Springfield",    "62701" })
+          .put("IN", new Object[]{"Indiana",          46001, 47997, "Indianapolis",   "46201" })
+          .put("IA", new Object[]{"Iowa",             50001, 52809, "Des Moines",     "50301" })
+          .put("KS", new Object[]{"Kansas",           66002, 67954, "Topeka",         "66601" })
+          .put("KY", new Object[]{"Kentucky",         40003, 42788, "Frankfort",      "40601" })
+          .put("LA", new Object[]{"Louisiana",        70001, 71497, "Baton Rouge",    "70801" })
+          .put("ME", new Object[]{"Maine",             3901,  4992, "Augusta",        "04330" })
+          .put("MD", new Object[]{"Maryland",         20331, 21930, "Annapolis",      "21401" })
+          .put("MA", new Object[]{"Massachusetts",     1001,  2791, "Boston",         "02108" })
+          .put("MI", new Object[]{"Michigan",         48001, 49971, "Lansing",        "48901" })
+          .put("MN", new Object[]{"Minnesota",        55001, 56763, "Saint Paul",     "55101" })
+          .put("MS", new Object[]{"Mississippi",      38601, 39776, "Jackson",        "39201" })
+          .put("MO", new Object[]{"Missouri",         63001, 65899, "Jefferson City", "65101" })
+          .put("MT", new Object[]{"Montana",          59001, 59937, "Helena",         "59601" })
+          .put("NE", new Object[]{"Nebraska",         68001, 69367, "Lincoln",        "68501" })
+          .put("NV", new Object[]{"Nevada",           88901, 89883, "Carson City",    "89701" })
+          .put("NH", new Object[]{"New Hampshire",     3031,  3897, "Concord",        "03301" })
+          .put("NJ", new Object[]{"New Jersey",        7001,  8989, "Trenton",        "08601" })
+          .put("NM", new Object[]{"New Mexico",       87001, 88441, "Santa Fe",       "87501" })
+          .put("NY", new Object[]{"New York",         10001, 14975, "Albany",         "12201" })
+          .put("NC", new Object[]{"North Carolina",   27006, 28909, "Raleigh",        "27601" })
+          .put("ND", new Object[]{"North Dakota",     58001, 58856, "Bismarck",       "58501" })
+          .put("OH", new Object[]{"Ohio",             43001, 45999, "Columbus",       "43201" })
+          .put("OK", new Object[]{"Oklahoma",         73001, 74966, "Oklahoma City",  "73101" })
+          .put("OR", new Object[]{"Oregon",           97001, 97920, "Salem",          "97301" })
+          .put("PA", new Object[]{"Pennsylvania",     15001, 19640, "Harrisburg",     "17101" })
+          .put("RI", new Object[]{"Rhode Island",      2801,  2940, "Providence",     "02901" })
+          .put("SC", new Object[]{"South Carolina",   29001, 29948, "Columbia",       "29201" })
+          .put("SD", new Object[]{"South Dakota",     57001, 57799, "Pierre",         "57501" })
+          .put("TN", new Object[]{"Tennessee",        37010, 38589, "Nashville",      "37201" })
+          .put("TX", new Object[]{"Texas",            75001, 79999, "Austin",         "73301" })
+          .put("UT", new Object[]{"Utah",             84001, 84784, "Salt Lake City", "84101" })
+          .put("VT", new Object[]{"Vermont",           5001,  5907, "Montpelier",     "05601" })
+          .put("VA", new Object[]{"Virginia",         20040, 24658, "Richmond",       "23218" })
+          .put("WA", new Object[]{"Washington",       98001, 99403, "Olympia",        "98501" })
+          .put("WV", new Object[]{"West Virginia",    24701, 26886, "Charleston",     "25301" })
+          .put("WI", new Object[]{"Wisconsin",        53001, 54990, "Madison",        "53701" })
+          .put("WY", new Object[]{"Wyoming",          82001, 83128, "Cheyenne",       "82001" })
           .build();  // @formatter:on //
+
+    /**
+     * Find the capital of the given state
+     *
+     * @param stateAbbreviation the state abbreviation
+     *
+     * @return the state's capital
+     */
+    public static String findCapital(final String stateAbbreviation)
+    {
+        return (String) STATE_INFORMATION.get(stateAbbreviation)[3];
+    }
+
+    /**
+     * Find the capital city zip code of the given state
+     *
+     * @param stateAbbreviation the state abbreviation
+     *
+     * @return the state capital's zip code
+     */
+    public static String findCapitalZip(final String stateAbbreviation)
+    {
+        return (String) STATE_INFORMATION.get(stateAbbreviation)[4];
+    }
 
     /**
      * Return a random US state abbreviation.
@@ -101,6 +141,9 @@ public class TestAddressUtil
 
     /**
      * Return a random zip code within the specified state.
+     * Specific values may not represent any real zip code,
+     * but the value is within the known range of zip codes
+     * for the given state.
      *
      * @param stateAbbreviation the state abbreviation
      *
